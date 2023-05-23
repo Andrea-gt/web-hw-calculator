07099c9a7b328260f5edb26e049acf593626a5e7
Andrea-gt <a.ximeram@gmail.com>
2023-05-23 10:14:13 -0600

    Basic operations

--- web-hw-calculator/src/App.jsx
+++ web-hw-calculator/src/App.jsx
@@ -1,77 +1,87 @@
-import React, { useState } from 'react';
-import styles from './App.module.css';
+import React, { useState } from 'react'
+import styles from './App.module.css'
 
-const digits_creator = (setDisplayValue) => {
+const digits_creator = (handleDigitClick) => {
   const digits = [];
   for (let i = 1; i < 10; i++) {
     digits.push(
-      <button key={i} onClick={() => handleDigitClick(setDisplayValue, i)}>
+      <button key={i} onClick={() => handleDigitClick(i)}>
         {i}
       </button>
-    );
+    )
   }
-  return digits;
-};
-
-const handleDigitClick = (setDisplayValue, digit) => {
-  setDisplayValue(prevValue => {
-    if (prevValue === 'ERROR') {
-      return String(digit);
-    }
-    const newValue = prevValue + digit;
-    if (parseInt(newValue) >= 999999999) {
-      return 'ERROR';
-    } else {
-      return newValue;
-    }
-  });
-};
+  return digits
+}
 
 function App() {
   const [displayValue, setDisplayValue] = useState('');
+  const [operator, setOperator] = useState('');
+  const [operand, setOperand] = useState('');
+
+  const handleDigitClick = (digit) => {
+    if (displayValue.length < 9 && !(digit === 0 && displayValue === '0')) {
+      setDisplayValue(prevValue => prevValue + digit.toString());
+    }
+  }
+
+  const handleOperatorClick = (operator) => {
+    setOperator(operator);
+    setOperand(displayValue);
+    setDisplayValue('');
+  }
+
+  const calculateResult = () => {
+    const operand1 = parseInt(operand);
+    const operand2 = parseInt(displayValue);
+    let result = 0;
+
+    switch (operator) {
+      case '+':
+        result = operand1 + operand2;
+        break;
+      case '-':
+        result = operand1 - operand2;
+        break;
+      case '*':
+        result = operand1 * operand2;
+        break;
+      default:
+        break;
+    }
+
+    setDisplayValue(result.toString());
+    setOperator('');
+    setOperand('');
+  }
 
   return (
     <>
       <div className={styles.calculator_container}>
-        <div className={styles.display}>{displayValue}</div>
+        <div className={styles.display}>
+          {displayValue}
+        </div>
         <div className={styles.operators}>
-          <button>
-            <img src='plus-sub.png' alt="Plus Subtraction" />
-          </button>
-          <button>
-            <img src='percentage.png' alt="Percentage" />
-          </button>
-          <button>
-            <img src='division.png' alt="Division" />
-          </button>
-          <button>
-            <img src='minus.png' alt="Minus" />
-          </button>
+          <button onClick={() => handleOperatorClick('+')}><img src='plus-sub.png' alt="plus-sub"/></button>
+          <button><img src='percentage.png' alt="percentage"/></button>
+          <button><img src='division.png' alt="division"/></button>
+          <button onClick={() => handleOperatorClick('-')}><img src='minus.png' alt="minus"/></button>
         </div>
         <div className={styles.digits_operators_container}>
           <div className={styles.digits}>
-            {digits_creator(setDisplayValue)}
-            <button onClick={() => handleDigitClick(setDisplayValue, '0')}>0</button>
-            <button className={styles.dot}>
-              <img src='dot.png' alt="Dot" />
-            </button>
+            {digits_creator(handleDigitClick)}
+            <button onClick={() => handleDigitClick(0)}>0</button>
+            <button className={styles.dot}><img src='dot.png' alt="dot"/></button>
             <button onClick={() => setDisplayValue('')}>AC</button>
           </div>
           <div className={styles.other_operators}>
-            <button>
-              <img src='multiply.png' alt="Multiply" />
-            </button>
-            <button>
-              <img src='plus-sign.png' alt="Plus Sign" />
-            </button>
-            <button className={styles.equal_button}>
-              <img src='equal.png' alt="Equal" />
-            </button>
+            <button onClick={() => handleOperatorClick('*')}><img src='multiply.png' alt="multiply"/></button>
+            <button onClick={() => handleOperatorClick('+')}><img src='plus-sign.png' alt="plus-sign"/></button>
+            <button className={styles.equal_button} onClick={calculateResult}><img src='equal.png' alt="equal"/></button>
           </div>
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
